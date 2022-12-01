ec4815f418f49e37c59df944d6aa57ccdeb1bc86
Kevinpizza123 <kap.sani@yavirac.edu.ec>
2022-12-01 10:56:37 -0500

    01 diciembre

--- proyecto1/src/app/pages/product/product.component.ts
+++ proyecto1/src/app/pages/product/product.component.ts
@@ -52,16 +52,16 @@ export class ProductComponent implements OnInit  {
   updateProduct(){
 
     const data = {
-   name : 'esfero' ,
-   price : 45 ,
-    description : 'utiles escolares', 
-    category : 1 ,
+   name : 'zapato' ,
+   price : 30 ,
+    description : 'calzado', 
+    category : 2 ,
     images : ["https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.blogs.es%2F5d6007%2Fluffy-en-one-piece%2F1366_2000.jpeg&imgrefurl=https%3A%2F%2Fwww.espinof.com%2Fanime%2Fsiempre-he-odiado-esa-corriente-creador-one-piece-no-queria-manga-combates-serios-revela-improbable-inspiracion-para-gear-fifth-luffy&tbnid=-CBJAUNzqc1anM&vet=12ahUKEwj40IeY4dj7AhUohuAKHVgmDIcQMygDegUIARDmAQ..i&docid=5DinRZOAuiZr2M&w=1211&h=681&q=luffy&ved=2ahUKEwj40IeY4dj7AhUohuAKHVgmDIcQMygDegUIARDmAQ" ,
   "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.geekmi.news%2F__export%2F1619631525888%2Fsites%2Fdebate%2Fimg%2F2021%2F04%2F28%2Fluffy1.jpg_1103262657.jpg&imgrefurl=https%3A%2F%2Fwww.geekmi.news%2Fseries%2FOne-Piece-Estos-son-los-apodos-que-le-ha-puesto-Monkey-D.-Luffy-a-los-demas-personajes-20210428-0013.html&tbnid=pBQnLBCiEj-ApM&vet=12ahUKEwj40IeY4dj7AhUohuAKHVgmDIcQMygIegUIARDwAQ..i&docid=cXDFf0OveG2OzM&w=1200&h=675&q=luffy&ved=2ahUKEwj40IeY4dj7AhUohuAKHVgmDIcQMygIegUIARDwAQ"]
 
     }
-   const url = "https://api.escuelajs.co/api/v1/product";
-    this.httpClient.post(url , data).subscribe(
+   const url = "https://api.escuelajs.co/api/v1/product/203";
+    this.httpClient.put(url , data).subscribe(
     response => {
     console.log(response);
 
