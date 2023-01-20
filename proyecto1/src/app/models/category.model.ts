38621e9659504b164087809d9319ccbcba6be012
Kevinpizza123 <kap.sani@yavirac.edu.ec>
2023-01-19 20:01:51 -0500

    adasda

--- proyecto1/src/app/models/category.model.ts
+++
@@ -1,5 +0,0 @@
-
-export interface CategoryModel{
-id : number ; // string o number 
-name : string ;
-}
--- proyecto1/src/app/models/product.model.ts
+++ proyecto1/src/app/models/product.model.ts
@@ -4,9 +4,11 @@ import { CategoryModel } from "./category.model";
 export interface ProductModel{
 id : number; // string o number 
 title : string ; 
-
 price : number ; 
 description : string ;
 category : CategoryModel;
 images : string [ ];
+
+
 }
+
